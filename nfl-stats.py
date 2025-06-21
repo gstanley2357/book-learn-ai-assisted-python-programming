795b91ec99a05f82558b798d6ab5f991e3af6f5c
gstanley2357 <gstanley2357@gmail.com>
2025-06-21 21:24:35 +0000

    add code for step 3

--- nfl-stats.py
+++ nfl-stats.py
@@ -71,3 +71,40 @@ Do not include Tom Brady because he wins too much
 for player in sorted(passing_yards, key=passing_yards.get, reverse=True):
     if player != 'Tom Brady':
         print(player, passing_yards[player])
+
+"""
+plot the players by their number of passing yards only for
+players with more than 4000 passing yards
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+# create a list of players
+players = []
+# create a list of passing yards
+yards = []
+# for each player in the dictionary
+for player in passing_yards:
+    # if the player has more than 4000 passing yards
+    if passing_yards[player] > 4000:
+        # add the player to the list of players
+        players.append(player)
+        # add the passing yards to the list of passing yards
+        yards.append(passing_yards[player])
+
+# create a numpy array of the passing yards
+yards = np.array(yards)
+# create a numpy array of the players
+players = np.array(players)
+# sort the players by the passing yards
+print(np.argsort(yards))
+players = players[np.argsort(yards)]
+# sort the passing yards
+yards = np.sort(yards)
+# create a figure
+plt.figure()
+# create a bar chart of the players and their passing yards
+plt.bar(players, yards)
+# rotate the x-axis labels
+plt.xticks(rotation=90)
+# show the plot
+plt.show()
